1373213407ad95de6f7ce48e9fda8eca3b56c25f
spookeyy <pangasmeshack@gmail.com>
2024-05-25 16:30:24 +0300

    test pass

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,11 @@
 
 class Dog:
     # Class body goes here
-
+    
     #Instance method definition
+    def bark(self):
+        print("Woof!")
+    
+    def sit(self):
+        print("The dog is sitting.")
     pass
